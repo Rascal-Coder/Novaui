50a016e04ceeff379440e3279e5b9103ee404a13
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2025-07-01 22:45:52 +0800

    chore: 优化primitives dialog

--- packages/primitives/src/components/dialog/dialog.tsx
+++ packages/primitives/src/components/dialog/dialog.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-use-before-define */
 import { hideOthers } from 'aria-hidden';
 import * as React from 'react';
 import { RemoveScroll } from 'react-remove-scroll';
@@ -17,6 +16,8 @@ import { createSlot } from '../slot';
  * ----------------------------------------------------------------------------------------------- */
 
 const DIALOG_NAME = 'Dialog';
+const CONTENT_NAME = 'DialogContent';
+const OVERLAY_NAME = 'DialogOverlay';
 
 type ScopedProps<P> = P & { __scopeDialog?: Scope };
 const [createDialogContext, createDialogScope] = createContextScope(DIALOG_NAME);
@@ -43,6 +44,105 @@ interface DialogProps {
   modal?: boolean;
 }
 
+/* ----------------------------------------------------------------------------------------------- */
+
+type DialogContentImplElement = React.ComponentRef<typeof DismissableLayer>;
+type DismissableLayerProps = React.ComponentPropsWithoutRef<typeof DismissableLayer>;
+type FocusScopeProps = React.ComponentPropsWithoutRef<typeof FocusScope>;
+interface DialogContentImplProps extends Omit<DismissableLayerProps, 'onDismiss'> {
+  /**
+   * When `true`, focus cannot escape the `Content` via keyboard, pointer, or a programmatic focus.
+   *
+   * @defaultValue false
+   */
+  trapFocus?: FocusScopeProps['trapped'];
+
+  /** Event handler called when auto-focusing on open. Can be prevented. */
+  onOpenAutoFocus?: FocusScopeProps['onMountAutoFocus'];
+
+  /** Event handler called when auto-focusing on close. Can be prevented. */
+  onCloseAutoFocus?: FocusScopeProps['onUnmountAutoFocus'];
+}
+
+const DIALOG_CONTENT_IMPL_NAME = 'DialogContentImpl';
+const DialogContentImpl = React.forwardRef<DialogContentImplElement, DialogContentImplProps>(
+  (props: ScopedProps<DialogContentImplProps>, forwardedRef) => {
+    const { __scopeDialog, trapFocus, onOpenAutoFocus, onCloseAutoFocus, ...contentProps } = props;
+    const context = useDialogContext(CONTENT_NAME, __scopeDialog);
+    const contentRef = React.useRef<HTMLDivElement>(null);
+    const composedRefs = useComposedRefs(forwardedRef, contentRef);
+
+    // Make sure the whole tree has focus guards as our `Dialog` will be
+    // the last element in the DOM (because of the `Portal`)
+    useFocusGuards();
+
+    return (
+      <>
+        <FocusScope
+          asChild
+          loop
+          trapped={trapFocus}
+          onMountAutoFocus={onOpenAutoFocus}
+          onUnmountAutoFocus={onCloseAutoFocus}
+        >
+          <DismissableLayer
+            aria-describedby={context.descriptionId}
+            aria-labelledby={context.titleId}
+            data-state={getState(context.open)}
+            id={context.contentId}
+            role="dialog"
+            {...contentProps}
+            ref={composedRefs}
+            onDismiss={() => context.onOpenChange(false)}
+          />
+        </FocusScope>
+        {/* {process.env.NODE_ENV !== 'production' && (
+          <>
+            <TitleWarning titleId={context.titleId} />
+            <DescriptionWarning
+              contentRef={contentRef}
+              descriptionId={context.descriptionId}
+            />
+          </>
+        )} */}
+      </>
+    );
+  }
+);
+
+DialogContentImpl.displayName = DIALOG_CONTENT_IMPL_NAME;
+
+type DialogOverlayImplElement = React.ComponentRef<typeof Primitive.div>;
+type PrimitiveDivProps = React.ComponentPropsWithoutRef<typeof Primitive.div>;
+interface DialogOverlayImplProps extends PrimitiveDivProps {}
+
+const Slot = createSlot('DialogOverlay.RemoveScroll');
+
+const DialogOverlayImpl = React.forwardRef<DialogOverlayImplElement, DialogOverlayImplProps>(
+  (props: ScopedProps<DialogOverlayImplProps>, forwardedRef) => {
+    const { __scopeDialog, ...overlayProps } = props;
+    const context = useDialogContext(OVERLAY_NAME, __scopeDialog);
+    return (
+      // Make sure `Content` is scrollable even when it doesn't live inside `RemoveScroll`
+      // ie. when `Overlay` and `Content` are siblings
+      <RemoveScroll
+        allowPinchZoom
+        as={Slot}
+        shards={[context.contentRef]}
+      >
+        <Primitive.div
+          data-state={getState(context.open)}
+          {...overlayProps}
+          ref={forwardedRef}
+          // We re-enable pointer-events prevented by `Dialog.Content` to allow scrolling the overlay.
+          style={{ pointerEvents: 'auto', ...overlayProps.style }}
+        />
+      </RemoveScroll>
+    );
+  }
+);
+
+DialogOverlayImpl.displayName = OVERLAY_NAME;
 const Dialog: React.FC<DialogProps> = (props: ScopedProps<DialogProps>) => {
   const { __scopeDialog, children, open: openProp, defaultOpen, onOpenChange, modal = true } = props;
   const triggerRef = React.useRef<HTMLButtonElement>(null);
@@ -156,8 +256,6 @@ DialogPortal.displayName = PORTAL_NAME;
  * DialogOverlay
  * ----------------------------------------------------------------------------------------------- */
 
-const OVERLAY_NAME = 'DialogOverlay';
-
 type DialogOverlayElement = DialogOverlayImplElement;
 interface DialogOverlayProps extends DialogOverlayImplProps {
   /**
@@ -185,77 +283,6 @@ const DialogOverlay = React.forwardRef<DialogOverlayElement, DialogOverlayProps>
 
 DialogOverlay.displayName = OVERLAY_NAME;
 
-type DialogOverlayImplElement = React.ComponentRef<typeof Primitive.div>;
-type PrimitiveDivProps = React.ComponentPropsWithoutRef<typeof Primitive.div>;
-interface DialogOverlayImplProps extends PrimitiveDivProps {}
-
-const Slot = createSlot('DialogOverlay.RemoveScroll');
-
-const DialogOverlayImpl = React.forwardRef<DialogOverlayImplElement, DialogOverlayImplProps>(
-  (props: ScopedProps<DialogOverlayImplProps>, forwardedRef) => {
-    const { __scopeDialog, ...overlayProps } = props;
-    const context = useDialogContext(OVERLAY_NAME, __scopeDialog);
-    return (
-      // Make sure `Content` is scrollable even when it doesn't live inside `RemoveScroll`
-      // ie. when `Overlay` and `Content` are siblings
-      <RemoveScroll
-        allowPinchZoom
-        as={Slot}
-        shards={[context.contentRef]}
-      >
-        <Primitive.div
-          data-state={getState(context.open)}
-          {...overlayProps}
-          ref={forwardedRef}
-          // We re-enable pointer-events prevented by `Dialog.Content` to allow scrolling the overlay.
-          style={{ pointerEvents: 'auto', ...overlayProps.style }}
-        />
-      </RemoveScroll>
-    );
-  }
-);
-
-DialogOverlayImpl.displayName = OVERLAY_NAME;
-/* -------------------------------------------------------------------------------------------------
- * DialogContent
- * ----------------------------------------------------------------------------------------------- */
-
-const CONTENT_NAME = 'DialogContent';
-
-type DialogContentElement = DialogContentTypeElement;
-interface DialogContentProps extends DialogContentTypeProps {
-  /**
-   * Used to force mounting when more control is needed. Useful when controlling animation with React animation
-   * libraries.
-   */
-  forceMount?: true;
-}
-
-const DialogContent = React.forwardRef<DialogContentElement, DialogContentProps>(
-  (props: ScopedProps<DialogContentProps>, forwardedRef) => {
-    const portalContext = usePortalContext(CONTENT_NAME, props.__scopeDialog);
-    const { forceMount = portalContext.forceMount, ...contentProps } = props;
-    const context = useDialogContext(CONTENT_NAME, props.__scopeDialog);
-    return (
-      <Presence present={forceMount || context.open}>
-        {context.modal ? (
-          <DialogContentModal
-            {...contentProps}
-            ref={forwardedRef}
-          />
-        ) : (
-          <DialogContentNonModal
-            {...contentProps}
-            ref={forwardedRef}
-          />
-        )}
-      </Presence>
-    );
-  }
-);
-
-DialogContent.displayName = CONTENT_NAME;
-
 /* ----------------------------------------------------------------------------------------------- */
 
 type DialogContentTypeElement = DialogContentImplElement;
@@ -275,17 +302,19 @@ const DialogContentModal = React.forwardRef<DialogContentTypeElement, DialogCont
 
     return (
       <DialogContentImpl
+        disableOutsidePointerEvents
         {...props}
         ref={composedRefs}
         // we make sure focus isn't trapped once `DialogContent` has been closed
         // (closed !== unmounted when animating out)
-        disableOutsidePointerEvents
+
         trapFocus={context.open}
-        onCloseAutoFocus={composeEventHandlers(props.onCloseAutoFocus, (event) => {
+        onFocusOutside={composeEventHandlers(props.onFocusOutside, event => event.preventDefault())}
+        onCloseAutoFocus={composeEventHandlers(props.onCloseAutoFocus, event => {
           event.preventDefault();
           context.triggerRef.current?.focus();
         })}
-        onPointerDownOutside={composeEventHandlers(props.onPointerDownOutside, (event) => {
+        onPointerDownOutside={composeEventHandlers(props.onPointerDownOutside, event => {
           const originalEvent = event.detail.originalEvent;
           const ctrlLeftClick = originalEvent.button === 0 && originalEvent.ctrlKey === true;
           const isRightClick = originalEvent.button === 2 || ctrlLeftClick;
@@ -296,9 +325,6 @@ const DialogContentModal = React.forwardRef<DialogContentTypeElement, DialogCont
         })}
         // When focus is trapped, a `focusout` event may still happen.
         // We make sure we don't trigger our `onDismiss` in such case.
-        onFocusOutside={composeEventHandlers(props.onFocusOutside, (event) =>
-          event.preventDefault()
-        )}
       />
     );
   }
@@ -308,6 +334,7 @@ DialogContentModal.displayName = CONTENT_NAME;
 
 /* ----------------------------------------------------------------------------------------------- */
 
+const CONTENT_NON_MODAL_NAME = 'DialogContentNonModal';
 const DialogContentNonModal = React.forwardRef<DialogContentTypeElement, DialogContentTypeProps>(
   (props: ScopedProps<DialogContentTypeProps>, forwardedRef) => {
     const context = useDialogContext(CONTENT_NAME, props.__scopeDialog);
@@ -362,74 +389,44 @@ const DialogContentNonModal = React.forwardRef<DialogContentTypeElement, DialogC
   }
 );
 
-DialogContentNonModal.displayName = CONTENT_NAME;
+DialogContentNonModal.displayName = CONTENT_NON_MODAL_NAME;
+/* -------------------------------------------------------------------------------------------------
+ * DialogContent
+ * ----------------------------------------------------------------------------------------------- */
 
-/* ----------------------------------------------------------------------------------------------- */
-
-type DialogContentImplElement = React.ComponentRef<typeof DismissableLayer>;
-type DismissableLayerProps = React.ComponentPropsWithoutRef<typeof DismissableLayer>;
-type FocusScopeProps = React.ComponentPropsWithoutRef<typeof FocusScope>;
-interface DialogContentImplProps extends Omit<DismissableLayerProps, 'onDismiss'> {
+type DialogContentElement = DialogContentTypeElement;
+interface DialogContentProps extends DialogContentTypeProps {
   /**
-   * When `true`, focus cannot escape the `Content` via keyboard, pointer, or a programmatic focus.
-   *
-   * @defaultValue false
+   * Used to force mounting when more control is needed. Useful when controlling animation with React animation
+   * libraries.
    */
-  trapFocus?: FocusScopeProps['trapped'];
-
-  /** Event handler called when auto-focusing on open. Can be prevented. */
-  onOpenAutoFocus?: FocusScopeProps['onMountAutoFocus'];
-
-  /** Event handler called when auto-focusing on close. Can be prevented. */
-  onCloseAutoFocus?: FocusScopeProps['onUnmountAutoFocus'];
+  forceMount?: true;
 }
 
-const DialogContentImpl = React.forwardRef<DialogContentImplElement, DialogContentImplProps>(
-  (props: ScopedProps<DialogContentImplProps>, forwardedRef) => {
-    const { __scopeDialog, trapFocus, onOpenAutoFocus, onCloseAutoFocus, ...contentProps } = props;
-    const context = useDialogContext(CONTENT_NAME, __scopeDialog);
-    const contentRef = React.useRef<HTMLDivElement>(null);
-    const composedRefs = useComposedRefs(forwardedRef, contentRef);
-
-    // Make sure the whole tree has focus guards as our `Dialog` will be
-    // the last element in the DOM (because of the `Portal`)
-    useFocusGuards();
-
+const DialogContent = React.forwardRef<DialogContentElement, DialogContentProps>(
+  (props: ScopedProps<DialogContentProps>, forwardedRef) => {
+    const portalContext = usePortalContext(CONTENT_NAME, props.__scopeDialog);
+    const { forceMount = portalContext.forceMount, ...contentProps } = props;
+    const context = useDialogContext(CONTENT_NAME, props.__scopeDialog);
     return (
-      <>
-        <FocusScope
-          asChild
-          loop
-          trapped={trapFocus}
-          onMountAutoFocus={onOpenAutoFocus}
-          onUnmountAutoFocus={onCloseAutoFocus}
-        >
-          <DismissableLayer
-            aria-describedby={context.descriptionId}
-            aria-labelledby={context.titleId}
-            data-state={getState(context.open)}
-            id={context.contentId}
-            role="dialog"
+      <Presence present={forceMount || context.open}>
+        {context.modal ? (
+          <DialogContentModal
             {...contentProps}
-            ref={composedRefs}
-            onDismiss={() => context.onOpenChange(false)}
+            ref={forwardedRef}
           />
-        </FocusScope>
-        {/* {process.env.NODE_ENV !== 'production' && (
-          <>
-            <TitleWarning titleId={context.titleId} />
-            <DescriptionWarning
-              contentRef={contentRef}
-              descriptionId={context.descriptionId}
-            />
-          </>
-        )} */}
-      </>
+        ) : (
+          <DialogContentNonModal
+            {...contentProps}
+            ref={forwardedRef}
+          />
+        )}
+      </Presence>
     );
   }
 );
 
-DialogContentImpl.displayName = CONTENT_NAME;
+DialogContent.displayName = CONTENT_NAME;
 
 /* -------------------------------------------------------------------------------------------------
  * DialogTitle
@@ -515,58 +512,6 @@ function getState(open: boolean) {
   return open ? 'open' : 'closed';
 }
 
-// const TITLE_WARNING_NAME = 'DialogTitleWarning';
-
-// const [WarningProvider, useWarningContext] = createContext(TITLE_WARNING_NAME, {
-//   contentName: CONTENT_NAME,
-//   titleName: TITLE_NAME,
-//   docsSlug: 'dialog'
-// });
-
-// type TitleWarningProps = { titleId?: string };
-
-// const TitleWarning: React.FC<TitleWarningProps> = ({ titleId }) => {
-//   const titleWarningContext = useWarningContext(TITLE_WARNING_NAME);
-
-//   const MESSAGE = `\`${titleWarningContext.contentName}\` requires a \`${titleWarningContext.titleName}\` for the component to be accessible for screen reader users.
-
-// If you want to hide the \`${titleWarningContext.titleName}\`, you can wrap it with our VisuallyHidden component.
-
-// For more information, see https://radix-ui.com/primitives/docs/components/${titleWarningContext.docsSlug}`;
-
-//   React.useEffect(() => {
-//     if (titleId) {
-//       const hasTitle = document.getElementById(titleId);
-//       if (!hasTitle) console.error(MESSAGE);
-//     }
-//   }, [MESSAGE, titleId]);
-
-//   return null;
-// };
-
-// const DESCRIPTION_WARNING_NAME = 'DialogDescriptionWarning';
-
-// type DescriptionWarningProps = {
-//   contentRef: React.RefObject<DialogContentElement | null>;
-//   descriptionId?: string;
-// };
-
-// const DescriptionWarning: React.FC<DescriptionWarningProps> = ({ contentRef, descriptionId }) => {
-//   const descriptionWarningContext = useWarningContext(DESCRIPTION_WARNING_NAME);
-//   const MESSAGE = `Warning: Missing \`Description\` or \`aria-describedby={undefined}\` for {${descriptionWarningContext.contentName}}.`;
-
-//   React.useEffect(() => {
-//     const describedById = contentRef.current?.getAttribute('aria-describedby');
-//     // if we have an id and the user hasn't set aria-describedby={undefined}
-//     if (descriptionId && describedById) {
-//       const hasDescription = document.getElementById(descriptionId);
-//       if (!hasDescription) console.warn(MESSAGE);
-//     }
-//   }, [MESSAGE, contentRef, descriptionId]);
-
-//   return null;
-// };
-
 const Root = Dialog;
 const Trigger = DialogTrigger;
 const Portal = DialogPortal;
@@ -577,6 +522,7 @@ const Description = DialogDescription;
 const Close = DialogClose;
 
 export {
+  // eslint-disable-next-line react-refresh/only-export-components
   createDialogScope,
   //
   Dialog,
